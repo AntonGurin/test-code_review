46556a51add8ffb893684a5bff72828280394a19
AntonGurin <79761911+AntonGurin@users.noreply.github.com>
2022-07-29 14:15:33 +0300

    add comments

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit'
 //import  { App }  from '../components/App/index'
 
+
+/*  Add to interface for everyone action
+    Create new dir 'types' in the src dir. Then keep all interfaces in types dir (1 level down src dir)
+    For everyone case we are create const with the same names. Write interfaces for each const.
+    Add file 'todos' to 'types' dir and keep here types about todo.
+    We could create file ./actions/const for export consts in ours actions.
+    All we need exporting from types dir, we are imports in other files e.x. index.ts
+    We can create another one case visability_filter
+    Case 'ADD_TODO' have to return object.assign({}) and add object.assign for each case.
+    On the one hand, keeping constants in separate files is not good for navigation or local logic but 
+    I think it's a pretty well solution for logic in general.
+*/
+
 export default configureStore({
     reducer: {
         list: (state = {todos: []}, action) => {
